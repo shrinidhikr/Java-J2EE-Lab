6cb0c7ec03f79fa7740b99a2cc191c26c62ba5ab
Shrinidhi KR <shrinidhi987@gmail.com>
2019-05-04 10:10:44 +0530

    Add files via upload

---
+++ JavaThreads/src/GetNumberThread.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class GetNumberThread extends Thread {
+
+public String number;
+private static String[] digit = new String[] {"zero","one","two","three","four","five","six","seven","eight","nine"};
+public void run() {
+Scanner s = new Scanner(System.in);
+System.out.println("Enter number with more than 4 digits: ");
+number = s.next();
+for(char c : number.toCharArray()) {
+if(c<48||c>57) {
+System.out.println("Invalid inputs");
+break;
+}
+System.out.print(digit[((int)c-48)]+" ");
+}
+System.out.print("\n");
+}
+}
---
+++ Telephone Directory/WebContent/Index.jsp
@@ -0,0 +1,13 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form  action="javaConnection" method="get"/>
+Enter name or phone:<input type="text" name="phone" /><br/>
+<input type="submit" />
+</form>
+<a href="insert.html;">insert into directory</a>
+</body>
+</html>
